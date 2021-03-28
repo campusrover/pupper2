90f7852b38f20ccbe0629ff297781096fcb0c384
Alexion <alexion@brandeis.edu>
2021-03-28 17:59:24 -0400

    Successful test of live feed into april tag detection.

--- camera_and_fids_test.py
+++ camera_and_fids_test.py
@@ -39,7 +39,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    image = cv2.flip(image, 0)
-
+    image = cv2.flip(image, -1)
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
     detect(detector, image)
 
