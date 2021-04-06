afa639baf03be15732f3af3c768ada8b585fdf28
Alexion <alexion@brandeis.edu>
2021-04-05 20:30:27 -0400

    Testing pupil_apriltags library.

--- camera_and_fids_test.py
+++ camera_and_fids_test.py
@@ -1,24 +1,23 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-import apriltag
+from pupil_apriltags import Detector
 import argparse
 import cv2
 import time
 
 def get_detector():
-    options = apriltag.DetectorOptions(families="tag36h11")
-    return apriltag.Detector(options)
+    return Detector(families="tag36h11")
 
 def detect(detector, img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    results = detector.detect(gray)
+    results = detector.detect(gray, estimate_tag_pose=True, camera_params=(3.6, 3.6, 0, 0), tag_size=0.065)
     print("[INFO] {} total AprilTags detected".format(len(results)))
     # loop over the AprilTag detection results
     for r in results:
         # extract the bounding box (x, y)-coordinates for the AprilTag
         # and convert each of the (x, y)-coordinate pairs to integers
         (ptA, ptB, ptC, ptD) = r.corners
-        print(r.corners)
+        print(r.pose_R)
 
 def init_camera():
 
