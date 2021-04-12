60669a64bd82ff9d943ecf527e5cf2db8fab1353
aalbert-dev <arjunalbert1@gmail.com>
2021-04-12 18:39:23 -0400

    Code reformat and transforms

--- camera_and_fids_test.py
+++ camera_and_fids_test.py
@@ -1,51 +1,70 @@
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from pupil_apriltags import Detector
 import argparse
 import cv2
 import time
+import math
 
-def get_detector():
-    return Detector(families="tag36h11")
+class Camera:
 
-def detect(detector, img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    results = detector.detect(gray, estimate_tag_pose=True, camera_params=(3.6, 3.6, 0, 0), tag_size=0.065)
-    print("[INFO] {} total AprilTags detected".format(len(results)))
-    # loop over the AprilTag detection results
-    for r in results:
-        # extract the bounding box (x, y)-coordinates for the AprilTag
-        # and convert each of the (x, y)-coordinate pairs to integers
-        (ptA, ptB, ptC, ptD) = r.corners
-        print(r.pose_R)
+    def __init__(self, camera_resolution=(640, 480), camera_framerate=32):
+        self.camera = PiCamera()
+        self.camera.resolution = camera_resolution
+        self.camera.framerate = camera_framerate
 
-def init_camera():
+        self.capture = PiRGBArray(self.camera, size=camera_resolution)
 
-    # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 32
-    return PiRGBArray(camera, size=(640, 480)), camera
+        self.detector = Detector(families="tag36h11")
 
-rawCapture, camera = init_camera()
+    def capture_continuous(self, format="bgr", use_video_port=True):
+        for frame in self.camera.capture_continuous(self.capture, format=format, use_video_port=use_video_port):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            image = frame.array
+            image = cv2.flip(image, -1)
+            # show the frame
+            cv2.imshow("Frame", image)
+            key = cv2.waitKey(1) & 0xFF
+            # clear the stream in preparation for the next frame
+            self.capture.truncate(0)
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
 
-detector = get_detector()
+            self.detect(image)
+
+    def detect(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        results = detector.detect(gray, estimate_tag_pose=True, camera_params=(3.6, 3.6, 0, 0), tag_size=0.065)
+        print("[INFO] {} total AprilTags detected".format(len(results)))
+        # loop over the AprilTag detection results
+        for r in results:
+            # extract the bounding box (x, y)-coordinates for the AprilTag
+            # and convert each of the (x, y)-coordinate pairs to integers
+            (ptA, ptB, ptC, ptD) = r.corners
+            print(r.pose_R)
  
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-    image = cv2.flip(image, -1)
-    # show the frame
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-    detect(detector, image)
-
+    def euler_from_quaternion(self, x, y, z, w):
+            """
+            Convert a quaternion into euler angles (roll, pitch, yaw)
+            roll is rotation around x in radians (counterclockwise)
+            pitch is rotation around y in radians (counterclockwise)
+            yaw is rotation around z in radians (counterclockwise)
+            """
+            t0 = +2.0 * (w * x + y * z)
+            t1 = +1.0 - 2.0 * (x * x + y * y)
+            roll_x = math.atan2(t0, t1)
+        
+            t2 = +2.0 * (w * y - z * x)
+            t2 = +1.0 if t2 > +1.0 else t2
+            t2 = -1.0 if t2 < -1.0 else t2
+            pitch_y = math.asin(t2)
+        
+            t3 = +2.0 * (w * z + x * y)
+            t4 = +1.0 - 2.0 * (y * y + z * z)
+            yaw_z = math.atan2(t3, t4)
+        
+            return roll_x, pitch_y, yaw_z # in radians
