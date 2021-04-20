b6e8b3d1403f7e3d072e6e3d09ef545b924bcee3
aalbert-dev <arjunalbert1@gmail.com>
2021-04-20 00:31:09 -0400

    removed print statements

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     while True:
         results = next(frames)
         if not results: 
-            print("No fiducials found")
+            #print("No fiducials found")
             continue
         for result in results:
             uid = result.tag_id
-            print("Found fiducial " + str(uid))
+            #print("Found fiducial " + str(uid))
             found_uid = env.find_obstacle_by_uid(uid)
             if found_uid == -1:
                 new_obstacle = deepcopy(obstacle)
