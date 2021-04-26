3b6428eb74087ebea6f8bc094ca4526dd5ce9d0b
aalbert-dev <arjunalbert1@gmail.com>
2021-04-26 19:05:06 -0400

    modified shape of ego agent

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ ego_agent = Shape()
 
 
 # length = 0.5m, width = 0.4m
-ego_agent_points = [Point(-0.25, 0.2), Point(0.25, 0.2), Point(0.25, -0.2), Point(-0.25, -0.2)]
+ego_agent_points = [Point(-0.2, 0.25), Point(0.2, 0.25), Point(0.2, -0.25), Point(-0.2, -0.25)]
 
 ego_agent.set_center(Point(0, 0))
 ego_agent.set_points(ego_agent_points)
